1814e55d05ff939bb7c1220de649789eb79fd2d8
Alan Lachkar <alan.lachkar@hotmail.fr>
2022-10-13 21:31:24 +0200

    feat installation  of react query library

--- src/api/helloworld.service.tsx
+++ src/api/helloworld.service.tsx
@@ -3,11 +3,18 @@ import { HelloWorldInterface } from '../types';
 import { API_URL } from '../utils/constants/constants';
 import api from './api';
 
-const getHelloWorld = () => {
+const delay = async (value: any, duration: number): Promise<any> =>
+    new Promise((resolve) => setTimeout(resolve, duration, value));
+
+const getHelloWorld = (): Promise<HelloWorldInterface> => {
     return api
         .get(`${API_URL}/helloworld.json`)
-        .then((response: AxiosResponse<HelloWorldInterface>) => {
-            return Promise.resolve(response.data as HelloWorldInterface);
+        .then(async (response: AxiosResponse<HelloWorldInterface>) => {
+            const delayData: HelloWorldInterface = await delay(
+                response.data,
+                2000
+            );
+            return Promise.resolve(delayData);
         });
 };
 export default {
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import ReactDOM from 'react-dom';
 
 import { HelloWorldPage } from './pages/HelloWorldPage';
+import QueryProvider from './providers/QueryProvider';
 const App = (): JSX.Element => {
     return <HelloWorldPage />;
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <QueryProvider>
+        <App />
+    </QueryProvider>,
+    document.getElementById('app')
+);
