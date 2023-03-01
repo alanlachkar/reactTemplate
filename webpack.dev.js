cced57f6ff020fb6e85417f0fec02f2eaf6d1a50
Alan Lachkar <alan.lachkar@hotmail.fr>
2023-03-01 22:40:31 +0100

    feat Checking the correct behavior when searching directly by URL

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,6 +10,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval', // Recommended choice for development builds with maximum performance.
   devServer: {
+    historyApiFallback: true,
     client: {
       overlay: {
         errors: true,
