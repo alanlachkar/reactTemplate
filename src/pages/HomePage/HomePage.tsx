3c45608210eb8c98561b136901dabb07cd057141
Alan Lachkar <alan.lachkar@hotmail.fr>
2023-03-01 18:47:32 +0100

    feat create HMI template

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,13 @@
-// React imports
-// Component imports
-// Utils imports
-// Css imports
-import styles from './HomePage.css';
-
-/**
- * Home Page component displaying when the user click on the 'My Player' icon
- * @returns A cards' list from json file, a player component
- */
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <header style={{ gridArea: 'nav', backgroundColor: 'green' }}>Nav</header>
+      <main style={{ gridArea: 'main', display: 'flex' }}>
+        <div style={{ backgroundColor: 'violet', flexGrow: 1 }}>Main 1</div>
+        <div style={{ backgroundColor: 'yellow', flexGrow: 1 }}>Main 2</div>
+      </main>
+    </>
+  );
 };
 
 export default HomePage;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,17 +5,46 @@ import { Outlet } from 'react-router-dom';
 import SelectTemplate from '../../components/SelectTemplate/SelectTemplate';
 // Utils imports
 import { ETemplate } from '../../utils/enums/enums';
+// Css imports
+import styles from './MainPage.css';
 
 const MainPage = (): JSX.Element => {
-  const [select, setSelect] = useState<ETemplate>(ETemplate.TOP_NAV);
+  const [selectNavigation, setSelectNavigation] = useState<ETemplate>(ETemplate.TOP_NAV);
+
   return (
     <>
-      <SelectTemplate onChangeSelect={setSelect} selectedValue={select} />
-      <div>
+      <div className={getTemplateStyle(selectNavigation)}>
+        {/* Each new child page (like HomePage) will need a grid-area css style to understand if it belongs to nav or main (see Main.css) */}
         <Outlet />
       </div>
+      <SelectTemplate
+        isFixedNavigation={selectNavigation === ETemplate.TOP_FIXED_NAV}
+        onChangeSelect={(newValue: string): void =>
+          setSelectNavigation(newValue as ETemplate)
+        }
+        selectedValue={selectNavigation}
+      />
     </>
   );
+};
+
+const getTemplateStyle = (selectValue: ETemplate) => {
+  let templateStyle;
+  switch (selectValue) {
+    case ETemplate.TOP_NAV:
+      templateStyle = styles.topBarNavigation;
+      break;
+    case ETemplate.TOP_FIXED_NAV:
+      templateStyle = styles.topFixedBarNavigation;
+      break;
+    case ETemplate.SIDE_NAV:
+      templateStyle = styles.sideBarNavigation;
+      break;
+    default:
+      templateStyle = undefined;
+      break;
+  }
+  return templateStyle;
 };
 
 export default MainPage;
