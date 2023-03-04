75c8ebd237b7dbdd7905fc8b795335f99adf4259
Alan Lachkar <alan.lachkar@hotmail.fr>
2023-03-04 16:49:34 +0100

    test Moving historyApiFallback into webpack.common file

--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true // Clean the output directory before emit.
   },
+  devServer: {
+    historyApiFallback: true
+  },
   module: {
     rules: [
       {
